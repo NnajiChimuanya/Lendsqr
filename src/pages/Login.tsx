38982969fc469b5f705992d731194c00098617c2
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-06 20:52:40 +0400

    Worked on the login page storing the email and password using useState, allso worked on the togglePassword functionality
    Signed-off-by: Chimuanya Nnaji <nnajichimuanya50@gmail.com>

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/login.scss";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [showPassword, setShowPassword] =
+    useState<React.SetStateAction<boolean>>(false);
+  const [email, setEmail] = useState<string | number>("");
+  const [password, setPassword] = useState<string | number>("");
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  console.log(email, password);
   return (
     <div className="login">
-      <h2>Heyyy Login</h2>
+      <div className="left">
+        <div className="logo-container">
+          <img className="logo" src="lendsqrLogo.png" alt="logo" />
+        </div>
+        <div className="pablo-container">
+          <img className="pablo" src="pablo.png" alt="pablo" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="header">
+          <h2>Welcome!</h2>
+          <p>Enter details to login.</p>
+        </div>
+
+        <form className="form">
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="email"
+            type={"text"}
+            placeholder="Email"
+          />
+
+          <div className="password-container">
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+              className="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+            />
+            <div className="toggle" onClick={togglePassword}>
+              SHOW
+            </div>
+          </div>
+
+          <p>
+            <Link className="link" to="/forgotPassword">
+              FORGOT PASSWORD?
+            </Link>
+          </p>
+
+          <button className="button">LOG IN</button>
+        </form>
+      </div>
     </div>
   );
 };
