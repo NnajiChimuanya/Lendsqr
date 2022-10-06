ec5d6f143a94260946441e54f925b1bc56c1d89f
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-07 00:50:51 +0400

    implemented the header UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./styles/app.scss";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./header.scss";
+import {
+  SearchOutlined,
+  NotificationsOutlined,
+  ArrowDropDown,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { Avatar } from "@mui/material";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img className="logo" src="lendsqrLogo.png" alt="logo" />
+      </div>
+      <div className="search-main">
+        <div className="search-container">
+          <input
+            className="search"
+            type={"text"}
+            placeholder="Search for anything"
+          />
+        </div>
+        <button>
+          <SearchOutlined className="search-icon" />
+        </button>
+      </div>
+      <div className="others">
+        <div className="docs">
+          <Link className="link" to={"/docs"}>
+            Docs
+          </Link>
+        </div>
+
+        <div className="notification">
+          <NotificationsOutlined className="notification-icon" />
+        </div>
+
+        <div className="user">
+          <Avatar className="user-avatar" src="boy.jpg" alt="boy" />
+          <div className="user-details">
+            <p> Adedeji </p>
+            <ArrowDropDown className="dropdown-icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
