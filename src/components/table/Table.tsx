7f0d83af0478f430096befa107a0767e966c8c3a
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-12 00:26:33 +0400

    Implement the filter modal

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,23 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {
-  FilterList,
-  ChevronLeft,
-  ChevronRight,
-  MoreVert,
-} from "@mui/icons-material";
+import { FilterList, MoreVert } from "@mui/icons-material";
 import "./table.scss";
 import { Link } from "react-router-dom";
-import {
-  UserTotal,
-  ActiveTotal,
-  LoanTotal,
-  SavingsTotal,
-  Eye,
-  UserTime,
-  UserCheck,
-} from "../../svgs";
+import { Eye, UserTime, UserCheck } from "../../svgs";
 import ITableHeaders from "../../interfaces/tableHeaders";
 import IUser from "../../interfaces/user";
+import { Button } from "@mui/material";
 
 const tableHeaders: ITableHeaders[] = [
   {
@@ -52,69 +40,131 @@ interface TableProps {
 
 const Table: React.FC<TableProps> = ({ paginatedPost }) => {
   const [hide, setHide] = useState<React.SetStateAction<boolean>>(true);
+  const [hideFilter, setHideFilter] =
+    useState<React.SetStateAction<boolean>>(true);
   const [clicked, setClicked] =
     useState<React.SetStateAction<number | undefined>>();
+
   return (
-    <table className="main-table">
-      <tr className="table-header">
-        {tableHeaders.map((item, id) => {
+    <div>
+      <div className={`${!hideFilter ? "filter-modal" : "hide-filter"}`}>
+        <div className="organization-container">
+          <label>Organization</label>
+          <select name="">
+            <option value={""}>select</option>
+            <option value={""}>Organization 1</option>
+            <option value={""}>Organization 2</option>
+            <option value={""}>Organization 3</option>
+          </select>
+        </div>
+
+        <div className="username">
+          <label>Username </label>
+          <input type="text" placeholder="User" />
+        </div>
+
+        <div className="email">
+          <label>Email </label>
+          <input type="email" placeholder="Email" />
+        </div>
+
+        <div className="date">
+          <label>Date </label>
+          <input type="date" placeholder="Date" />
+        </div>
+
+        <div className="number">
+          <label>Phone Number </label>
+          <input type="number" placeholder="Phone Number" />
+        </div>
+
+        <div className="status">
+          <label>Status </label>
+          <select name="" id="">
+            <option value="">Select</option>
+            <option value="">Active</option>
+            <option value="">Inactive</option>
+            <option value="">Pending</option>
+            <option value="">Blacklisted</option>
+          </select>
+        </div>
+
+        <div className="buttons">
+          <Button onClick={() => setHideFilter(!hideFilter)} className="reset">
+            Reset
+          </Button>
+          <Button onClick={() => setHideFilter(!hideFilter)} className="filter">
+            Filter
+          </Button>
+        </div>
+      </div>
+
+      <table className="main-table">
+        <tr className="table-header">
+          {tableHeaders.map((item, id) => {
+            return (
+              <th className="table-header" key={id}>
+                <div>
+                  {item.name}
+                  <FilterList
+                    onClick={() => setHideFilter(!hideFilter)}
+                    className="filter-icon"
+                  />
+                </div>
+              </th>
+            );
+          })}
+        </tr>
+        {paginatedPost?.map((item: IUser, index: number) => {
+          let { orgName, userName, email, phoneNumber, createdAt, id } = item;
+          let userId: number = Number(id);
           return (
-            <th className="table-header" key={id}>
-              <div>
-                {item.name}
-                <FilterList className="filter-icon" />
-              </div>
-            </th>
+            <tr className="table-data" key={id}>
+              <td>{orgName}</td>
+              <td>{userName}</td>
+              <td>{email}</td>
+              <td>{phoneNumber}</td>
+              <td>{new Date(createdAt).toUTCString()}</td>
+              <td>
+                <div className="status">
+                  <p>Status</p>
+                  <MoreVert
+                    className="icon"
+                    onClick={() => {
+                      setClicked(userId);
+                      setHide(!hide);
+                    }}
+                  />
+                </div>
+
+                <div
+                  className={`${
+                    !hide && userId === clicked ? "modal" : "hide"
+                  }`}
+                >
+                  <div className="modal-items">
+                    <Eye />
+                    <Link className="link" to={`userDetails/${userId}`}>
+                      <p>View Details</p>
+                    </Link>
+                  </div>
+
+                  <div className="modal-items">
+                    <UserTime />
+                    <p>Blacklist User</p>
+                  </div>
+
+                  <div className="modal-items">
+                    <UserCheck />
+                    <p> Activate User</p>
+                  </div>
+                </div>
+              </td>
+            </tr>
           );
         })}
-      </tr>
-      {paginatedPost?.map((item: IUser, index: number) => {
-        let { orgName, userName, email, phoneNumber, createdAt, id } = item;
-        let userId: number = Number(id);
-        return (
-          <tr className="table-data" key={id}>
-            <td>{orgName}</td>
-            <td>{userName}</td>
-            <td>{email}</td>
-            <td>{phoneNumber}</td>
-            <td>{createdAt}</td>
-            <td>
-              <div className="status">
-                <p>Status</p>
-                <MoreVert
-                  className="icon"
-                  onClick={() => {
-                    setClicked(userId);
-                    setHide(!hide);
-                  }}
-                />
-              </div>
-
-              <div
-                className={`${!hide && userId === clicked ? "modal" : "hide"}`}
-              >
-                <div className="modal-items">
-                  <Eye />
-                  <Link className="link" to={`userDetails/${userId}`}>
-                    <p>View Details</p>
-                  </Link>
-                </div>
-
-                <div className="modal-items">
-                  <UserTime />
-                  <p>Blacklist User</p>
-                </div>
-
-                <div className="modal-items">
-                  <UserCheck />
-                  <p> Activate User</p>
-                </div>
-              </div>
-            </td>
-          </tr>
-        );
-      })}
-    </table>
+      </table>
+    </div>
   );
 };
 
